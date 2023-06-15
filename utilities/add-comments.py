763e9f6049ad493bf3460355abaac0170902a5aa
lunajos <28819281+lunajos@users.noreply.github.com>
2023-06-15 11:20:08 -0400

    Loop through roles and update tag names

--- utilities/add-comments.py
+++ utilities/add-comments.py
@@ -2,6 +2,9 @@
 
 import os
 
+ansibleDir = "/home/jluna/develop/ansible/playbooks/"
+rolesDir = "/home/jluna/develop/ansible/playbooks/roles/"
+
 def insert_comment(file_path):
     with open(file_path, 'r+') as file:
         lines = file.readlines()
@@ -10,10 +13,10 @@ def insert_comment(file_path):
             if "- name" in line:
                 name = line.split("- name", 1)[1]
                 file.write("# @name: " + name)
-                file.write("# @comment: \n")
-                file.write("# @code: \n")
-                file.write("# @input:\n")
-                file.write("# @output:\n")
+                file.write("# @para: this is some text that describe the purpose of the plabook\n")
+                file.write("# @comment: This is a comment\n")
+                file.write("# @code: #! /usr/bin/bash \\n\\n echo \"this is a test\" \n")
+                #file.write("# @output: \n")
             if "- hosts" in line:
                 hosts = line.split("- hosts:", 1)[1]
                 file.write("# @hosts: " + hosts)
@@ -21,4 +24,20 @@ def insert_comment(file_path):
         file.truncate()
 
 # Example usage
-insert_comment('scripts/ansible-clamav.yml')
+#insert_comment('/home/jluna/develop/ansible/roles/tasks/')
+
+for filename in os.listdir(rolesDir):
+    tasks = os.path.join(rolesDir, filename + "/tasks/")
+    if os.path.exists(tasks):
+        for task in os.listdir(tasks):
+            if task.endswith(".yml") or task.endswith(".yaml"):
+                rolesFilePath = os.path.join(tasks, task)
+                print (rolesFilePath)
+                insert_comment(rolesFilePath)
+
+
+for filename in os.listdir(ansibleDir):
+    if filename.endswith(".yml") or filename.endswith(".yaml"):
+        ansibleFilePath = os.path.join(ansibleDir, filename)
+        print (ansibleFilePath)
+        insert_comment(ansibleFilePath)
